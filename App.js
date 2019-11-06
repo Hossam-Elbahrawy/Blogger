756f14195c44cecb561aff13448368e81a5f25b0
Hossam Elbahrawy <hossam@hayasolutions.com>
2019-11-06 02:12:33 +0200

    using reducer

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
-import { BlogProvider } from './src/context/blogContext';
+import { Provider } from './src/context/BlogContext';
+
 const navigator = createStackNavigator(
   {
     Index: IndexScreen
@@ -19,8 +20,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,28 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import BlogContext from '../context/blogContext';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
-  console.log('value', value);
+  const { state, addBlogPost } = useContext(Context);
+
   return (
     <View>
-      <Text> Index Screen</Text>
+      <Text style={{ alignSelf: 'center', fontSize: 25 }}> Index Screen</Text>
+      <Button title='Add Post' onPress={() => addBlogPost()} />
+      <FlatList
+        data={state}
+        keyExtractor={post => post.title}
+        renderItem={({ item }) => (
+          <Text style={styles.postTitleStyle}> {item.title}</Text>
+        )}
+      />
     </View>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  postTitleStyle: {
+    fontSize: 20
+  }
+});
 
 export default IndexScreen;
